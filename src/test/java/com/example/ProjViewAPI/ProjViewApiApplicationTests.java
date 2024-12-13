03506d2df438293fb5c850991968ccfd6a5135cf
Daniel Chovanec <daniel.chovanec.3@student.tuke.sk>
2024-12-14 00:37:56 +0100

    forgot to delete dev @Value

--- src/test/java/com/example/ProjViewAPI/ProjViewApiApplicationTests.java
+++ src/test/java/com/example/ProjViewAPI/ProjViewApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProjViewApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
