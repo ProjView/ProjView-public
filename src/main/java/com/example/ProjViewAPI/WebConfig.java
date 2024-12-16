a67eeb706af62db90cae60955e3faaf4963b9279
Matúš Šándor <matus.sandor@student.tuke.sk>
2024-12-16 21:01:41 +0100

    Update file WebConfig.java

--- src/main/java/com/example/ProjViewAPI/WebConfig.java
+++ src/main/java/com/example/ProjViewAPI/WebConfig.java
@@ -13,12 +13,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         // Forward root and any single-level path to index.html
-       registry.addViewController("/").setViewName("forward:/index.html");
+//        registry.addViewController("/").setViewName("forward:/index.html");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Serve static content from "classpath:/static/"
-       registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
